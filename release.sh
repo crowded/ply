7d2c9b6496a0c61e99631c99b570166ab1916810
Nino Floris <mail@ninofloris.com>
2020-08-21 15:18:04 +0200

    Bump version

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 PLY_NUGET_KEY=${PLY_NUGET_KEY?env var with nuget credentials is not set}
 version="$(xmllint --xpath '//Project/PropertyGroup/Version/text()' Ply.fsproj)"
 
-dotnet pack Ply.fsproj -c Release -p:ContinuousIntegrationBuild=true
-dotnet nuget push "bin/Release/Ply.${version}.nupkg" -s nuget.org --interactive -k $PLY_NUGET_KEY
+dotnet pack Ply.fsproj -c Release -p:ContinuousIntegrationBuild=true \
+    && dotnet nuget push "bin/Release/Ply.${version}.nupkg" -s nuget.org --interactive -k $PLY_NUGET_KEY
 
